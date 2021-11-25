d16b6ae3a870581d71fa2efdae6a0d874598d08e
Abdullah Ahmad <61104635+abdullah2808@users.noreply.github.com>
2021-11-24 21:02:19 -0600

    Cleaned up processing file

--- src/application/Process.py
+++ src/application/Process.py
@@ -19,25 +19,7 @@ def calculateDistance(signalStrength, frequency):
     distance = 10 ** ((27.55 - (20 * np.log10(frequency)) + np.abs(signalStrength))/20)
     return distance
 
-print(calculateDistance(signalstrength[2], frequency))
-
 def calculateLocation(signalStrength, frequency, receiverA, receiverB, receiverC):
-    distanceA = calculateDistance(signalStrength[0], frequency)
-    distanceB = calculateDistance(signalStrength[1], frequency)
-    distanceC = calculateDistance(signalStrength[2], frequency)
-    distanceAx = np.cos(angles[0]) * distanceA
-    distanceAy = np.sin(angles[0]) * distanceA
-    distanceBx = np.cos(angles[1]) * distanceB
-    distanceBy = np.sin(angles[1]) * distanceB
-    distanceCx = np.cos(angles[2]) * distanceC
-    distanceCy = np.sin(angles[2]) * distanceC
-    locationA = [receiverA[0] + distanceAx, receiverA[1] + distanceAy]
-    locationB = [receiverB[0] + distanceBx, receiverB[1] + distanceBy]
-    locationC = [receiverC[0] + distanceCx, receiverC[1] + distanceCy]
-    transmitterLocation = [(locationA[0] + locationB[0] + locationC[0]) / 3, (locationA[1] + locationB[1] + locationC[1]) / 3]
-    return transmitterLocation
-
-def calculateLocation2(signalStrength, frequency, receiverA, receiverB, receiverC):
     distanceA = calculateDistance(signalStrength[0], frequency)
     distanceB = calculateDistance(signalStrength[1], frequency)
     distanceC = calculateDistance(signalStrength[2], frequency)
@@ -46,6 +28,6 @@ def calculateLocation2(signalStrength, frequency, receiverA, receiverB, receiver
     CtoT = geopy.distance.distance(meters=distanceC).destination((receiverC[0], receiverC[1]), bearing=angles[2])
     transmitterLocation = [(AtoT.latitude + BtoT.latitude + CtoT.latitude) / 3, (AtoT.longitude + BtoT.longitude + CtoT.longitude) / 3]
     return transmitterLocation
-transmitterLocation = calculateLocation2(signalstrength, frequency, receiverA, receiverB, receiverC)
+transmitterLocation = calculateLocation(signalstrength, frequency, receiverA, receiverB, receiverC)
 
 print(transmitterLocation)
